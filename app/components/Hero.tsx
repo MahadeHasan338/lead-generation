0fcb43bf55a6773f0702d8acc96b5028dc29349f
MahadeHasan338 <mahadeh338@gmail.com>
2024-02-22 23:17:43 +0600

    curve

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,7 @@ import { didact_gothic } from "@/app/fonts";
 const Hero = () => {
   return (
     <section id="home" className="default-container mt-2">
-      <div className="grid grid-cols-1 xl:grid-cols-2 space-y-14 xl:space-x-[30px] text-center xl:text-left xl:items-center pb-20">
+      <div className="grid grid-cols-1 xl:grid-cols-2 space-y-14 xl:space-x-[30px] text-center xl:text-left xl:items-center pb-16">
         <div
           className={`${didact_gothic.className} flex flex-col space-y-5 xl:space-y-8`}
         >
@@ -29,20 +29,6 @@ const Hero = () => {
         </div>
         <Image src={hero} alt="hero"></Image>
       </div>
-
-      {/* <div className="custom-shape-divider-bottom-1708545494">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="shape-fill"
-          ></path>
-        </svg>
-      </div> */}
     </section>
   );
 };
