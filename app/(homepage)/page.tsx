5e433e697a627eb523dfec235efd6c3471cec498
MahadeHasan338 <mahadeh338@gmail.com>
2024-03-02 18:27:10 +0600

    whatsapp

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -7,11 +7,13 @@ import Testimonials from "./components/Testimonials";
 import Investigation from "./components/Investigation";
 import MaximitePotential from "./components/MaximitePotential";
 import Qualification from "./components/Qualification";
+import WhatsAppButton from "../components/WhatsAppButton";
 
 const Home = async () => {
+  const phoneNumber = "01728819680";
+  const message = "Hello Pearl Lemon Leads!";
   return (
     <>
-      
       <Hero />
       <Curves />
       <Services />
@@ -21,6 +23,9 @@ const Home = async () => {
       <Investigation />
       <MaximitePotential />
       <Qualification />
+      <div className="fixed right-5 bottom-5">
+        <WhatsAppButton phoneNumber={phoneNumber} message={message} />
+      </div>
     </>
   );
 };
